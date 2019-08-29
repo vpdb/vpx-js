c83b8df7b16cd6a0c308bc54b1b544f979831549
freezy <freezy@xbmc.org>
2019-08-29 16:45:38 +0200

    chore: Rebase to master and switch to escodegen.

--- lib/scripting/transpiler.ts
+++ lib/scripting/transpiler.ts
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-import { generate } from 'astring';
+import { generate } from 'escodegen';
 import { Program } from 'estree';
 import { Grammar, Parser } from 'nearley';
 import { Table } from '../vpt/table/table';
