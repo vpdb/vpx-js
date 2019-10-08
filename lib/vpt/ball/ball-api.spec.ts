42a202408aa7cc77e39fa6ac2d07f4233af4a961
freezy <freezy@kodi.tv>
2019-10-09 00:30:54 +0200

    ball: Add API spec.

---
+++ lib/vpt/ball/ball-api.spec.ts
@@ -0,0 +1,54 @@
+/*
+ * VPDB - Virtual Pinball Database
+ * Copyright (C) 2019 freezy <freezy@vpdb.io>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import * as chai from 'chai';
+import { expect } from 'chai';
+import sinonChai = require('sinon-chai');
+import { createBall } from '../../../test/physics.helper';
+import { ThreeHelper } from '../../../test/three.helper';
+import { Player } from '../../game/player';
+import { NodeBinaryReader } from '../../io/binary-reader.node';
+import { Table } from '../table/table';
+
+/* tslint:disable:no-unused-expression */
+chai.use(sinonChai);
+const three = new ThreeHelper();
+
+describe('The VPinball ball API', () => {
+
+	let table: Table;
+	let player: Player;
+
+	beforeEach(async () => {
+		table = await Table.load(new NodeBinaryReader(three.fixturePath('table-empty.vpx')));
+		player = new Player(table).init();
+	});
+
+	it('should correctly read and write the properties', async () => {
+		const ball = createBall(player, 500, 2100, 0);
+		const api = ball.getApi();
+
+		api.X = 23;
+		api.Y = 32;
+
+		expect(api.X).to.equal(23);
+		expect(api.Y).to.equal(32);
+	});
+
+});
