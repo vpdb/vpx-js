716fea0f5ecd92524431f6c99a110e70e31f146e
freezy <freezy@kodi.tv>
2019-10-20 11:57:08 +0200

    surface: Add material, texture and visibility of side and top to state.

--- lib/vpt/surface/surface-state.ts
+++ lib/vpt/surface/surface-state.ts
@@ -24,35 +24,74 @@ export class SurfaceState extends ItemState {
 
 	public static readonly POOL = new Pool(SurfaceState);
 
-	public material!: string;
+	public heightTop!: number;
+
+	public topVisible: boolean = true;
+	public topMaterial?: string;
+	public topTexture?: string;
+
+	public sideVisible: boolean = true;
+	public sideMaterial?: string;
+	public sideTexture?: string;
+
+	get isVisible() { return this.topVisible || this.sideVisible; }
+	set isVisible(v) { /* not used in abstract */ }
 
 	public constructor() {
 		super();
 	}
 
-	public static claim(name: string, material: string, isVisible: boolean): SurfaceState {
+	public static claim(name: string, heightTop: number,
+						topVisible: boolean, topMaterial: string | undefined, topTexture: string | undefined,
+						sideVisible: boolean, sideMaterial: string | undefined, sideTexture: string | undefined): SurfaceState {
+
 		const state = SurfaceState.POOL.get();
 		state.name = name;
-		state.material = material;
-		state.isVisible = isVisible;
+		state.heightTop = heightTop;
+		state.topVisible = topVisible;
+		state.topMaterial = topMaterial;
+		state.topTexture = topTexture;
+		state.sideVisible = sideVisible;
+		state.sideMaterial = sideMaterial;
+		state.sideTexture = sideTexture;
 		return state;
 	}
 
 	public clone(): SurfaceState {
 		return SurfaceState.claim(
 			this.name,
-			this.material,
-			this.isVisible,
+			this.heightTop,
+			this.topVisible,
+			this.topMaterial,
+			this.topTexture,
+			this.sideVisible,
+			this.sideMaterial,
+			this.sideTexture,
 		);
 	}
 
 	public diff(state: SurfaceState): SurfaceState {
 		const diff = this.clone();
-		if (diff.material === state.material) {
-			delete diff.material;
+		if (diff.heightTop === state.heightTop) {
+			delete diff.heightTop;
 		}
-		if (diff.isVisible === state.isVisible) {
-			delete diff.isVisible;
+		if (diff.topVisible === state.topVisible) {
+			delete diff.topVisible;
+		}
+		if (diff.topMaterial === state.topMaterial) {
+			delete diff.topMaterial;
+		}
+		if (diff.topTexture === state.topTexture) {
+			delete diff.topTexture;
+		}
+		if (diff.sideVisible === state.sideVisible) {
+			delete diff.sideVisible;
+		}
+		if (diff.sideMaterial === state.sideMaterial) {
+			delete diff.sideMaterial;
+		}
+		if (diff.sideTexture === state.sideTexture) {
+			delete diff.sideTexture;
 		}
 		return diff;
 	}
@@ -66,7 +105,12 @@ export class SurfaceState extends ItemState {
 		if (!state) {
 			return false;
 		}
-		return state.material === this.material
-			&& state.isVisible === this.isVisible;
+		return state.heightTop === this.heightTop
+			&& state.topVisible === this.topVisible
+			&& state.topMaterial === this.topMaterial
+			&& state.topTexture === this.topTexture
+			&& state.sideVisible === this.sideVisible
+			&& state.sideMaterial === this.sideMaterial
+			&& state.sideTexture === this.sideTexture;
 	}
 }
