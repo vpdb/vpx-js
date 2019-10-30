e8c7e167ab82ad42da72731c9e44d15845e7126c
freezy <freezy@kodi.tv>
2019-10-30 18:04:30 +0100

    bin: Add folder support to script extracter.

--- bin/vptscript.ts
+++ bin/vptscript.ts
@@ -18,8 +18,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-import { existsSync } from 'fs';
-import { resolve } from 'path';
+import { closeSync, existsSync, futimesSync, lstatSync, openSync, readdirSync, statSync, writeFileSync } from 'fs';
+import { basename, dirname, resolve } from 'path';
 import { NodeBinaryReader } from '../lib/io/binary-reader.node';
 import { Table } from '../lib/vpt/table/table';
 
@@ -29,22 +29,52 @@ import { Table } from '../lib/vpt/table/table';
 
 		const argSrc = process.argv[2];
 		if (!argSrc) {
-			console.log('Prints the table script of a Visual Pinball table.\n\nUSAGE: vptscript <source.vpx>\n');
+			console.log('Prints or saves the table script of a Visual Pinball table.\n\nUSAGE: vptscript <source.vpx | folder> [--save]\n');
 			return;
 		}
-		if (!/\.vp[xt]$/i.test(argSrc)) {
-			throw new Error('First argument must be a .vpx or .vpt file.');
-		}
+
 		const vpxPath = resolve(argSrc);
 		if (!existsSync(vpxPath)) {
-			throw new Error(`The file "${vpxPath}" does not exist.`);
+			throw new Error(`The path "${vpxPath}" does not exist.`);
 		}
 
-		const vpt = await Table.load(new NodeBinaryReader(vpxPath), { loadTableScript: true });
-		console.log(vpt.getTableScript());
+		const writeToFile = process.argv.includes('--save');
+		const isFolder = lstatSync(vpxPath).isDirectory();
+		let vpxFiles: string[];
+		if (isFolder) {
+			vpxFiles = readdirSync(vpxPath)
+				.filter(f => /\.vp[xt]$/i.test(f))
+				.map(f => resolve(vpxPath, f));
+		} else {
+			if (!/\.vp[xt]$/i.test(vpxPath)) {
+				throw new Error('File must be a .vpx or .vpt file.');
+			}
+			vpxFiles = [ vpxPath ];
+		}
+		for (const vpxFile of vpxFiles) {
+			const vpt = await Table.load(new NodeBinaryReader(vpxFile), { loadTableScript: true, tableDataOnly: true });
+
+			if (writeToFile) {
+				const destPath = dirname(vpxFile);
+				const destName = basename(vpxFile);
+				const destFile = resolve(destPath, destName.substr(0, destName.length - 3) + 'vbs');
+
+				console.log('[vptscript] Writing to "%s".', destFile);
+				writeFileSync(destFile, vpt.getTableScript());
+
+				// update timestamp
+				const srcStat = statSync(vpxFile);
+				const destFs = openSync(destFile, 'r+');
+				futimesSync(destFs, srcStat.atime, srcStat.mtime);
+				closeSync(destFs);
+
+			} else {
+				console.log(vpt.getTableScript());
+			}
+		}
 
 	} catch (err) {
-		console.error(`ERROR: ${err.message}`);
+		console.error(err);
 
 	} finally {
 		process.exit();
