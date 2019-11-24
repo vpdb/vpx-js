25e4f582f1b0a4f17df8d703172a03d6888fad59
freezy <freezy@kodi.tv>
2019-11-24 01:12:12 +0100

    sound: Read format and offsets.

--- lib/vpt/sound/pin-sound-data.ts
+++ lib/vpt/sound/pin-sound-data.ts
@@ -23,10 +23,21 @@ import { ItemData } from '../item-data';
 
 export class PinSoundData extends ItemData {
 
-	public path: string = '';
-	public internalName: string = '';
+	public path!: string;
+	public internalName!: string;
+	public wfx!: WaveFormat;
 
-	private pos = 0;
+	/**
+	 * Sound data start
+	 */
+	public offset: number = 0;
+
+	/**
+	 * Sound data length
+	 */
+	public len: number = 0;
+
+	private i = 0;
 
 	public static async fromStorage(storage: Storage, itemName: string): Promise<PinSoundData> {
 		const soundData = new PinSoundData(itemName);
@@ -36,11 +47,16 @@ export class PinSoundData extends ItemData {
 
 	public async stream(result: ReadResult): Promise<number | null> {
 		const len = this.getInt(result.data);
-		this.pos++;
-		switch (this.pos) {
+		this.i++;
+		switch (this.i) {
 			case 1: this.name = this.getString(result.data, len + 4); return len + 4;
 			case 2: this.path = this.getString(result.data, len + 4); return len + 4;
 			case 3: this.internalName = this.getString(result.data, len + 4); return len + 4;
+			case 4: this.wfx = new WaveFormat(result.data); return WaveFormat.STRUCT_SIZE;
+			case 5:
+				this.len = this.getInt(result.data);
+				this.offset = result.storageOffset + 4;
+				return this.len + 4;
 		}
 		return null;
 	}
@@ -48,4 +64,57 @@ export class PinSoundData extends ItemData {
 	public constructor(itemName: string) {
 		super(itemName);
 	}
+}
+
+export class WaveFormat {
+
+	public static readonly STRUCT_SIZE = 18;
+
+	/**
+	 * Format type
+	 */
+	public formatTag: number;
+
+	/**
+	 * Number of channels (i.e. mono, stereo...)
+	 */
+	public channels: number;
+
+	/**
+	 * Sample rate
+	 */
+	public samplesPerSec: number;
+
+	/**
+	 * For buffer estimation
+	 */
+	public avgBytesPerSec: number;
+
+	/**
+	 * Block size of data
+	 */
+	public blockAlign: number;
+	/**
+	 * Number of bits per sample of mono data
+	 */
+	public bitsPerSample: number;
+
+	/**
+	 * The count in bytes of the size of extra information (after cbSize)
+	 */
+	public cbSize: number;
+
+	constructor(data: Buffer) {
+		this.formatTag = data.readUInt16LE(0);
+		this.channels = data.readUInt16LE(2);
+		this.samplesPerSec = data.readUInt32LE(4);
+		this.avgBytesPerSec = data.readUInt32LE(8);
+		this.blockAlign = data.readUInt16LE(12);
+		this.bitsPerSample = data.readUInt16LE(14);
+		this.cbSize = data.readUInt16LE(16);
+	}
+}
+
+function readUInt64LE(buffer: Buffer, offset: number) {
+	return buffer.readUInt32LE(offset) + 0x100000000 * buffer.readUInt32LE(offset + 4);
 }
