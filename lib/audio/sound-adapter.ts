1c7e70e5cc136adbdafda4228cdf6105520a16fe
michuNEEO <michael@neeo.com>
2019-11-24 23:14:18 +0100

    document audio api

--- lib/audio/sound-adapter.ts
+++ lib/audio/sound-adapter.ts
@@ -1,7 +1,27 @@
 export interface ISoundAdapter {
 
+	/**
+	 * Play an audio sample OR modify an already playing sample (increase/decrease the frequency of an already playing samples)
+	 * @param sample
+	 */
 	playSound(sample: PlaybackSettings): void;
 
+	/**
+	 * stop playing sample
+	 * @param sampleName
+	 */
+	stopSound(sampleName: string): void;
+
+	//TODO unclear if we need to handle play music or if we can reuse sound
+	//PlayMusic(string, float volume) - volume 0..1
+	//MusicVolume(float volume) - 0..1
+	//EndMusic()
+
+	/**
+	 * TODO UNCLEAR - but we need to define how to preload audio sample
+	 * @param sampleName internal identifier
+	 * @param url where to download this data.. TODO might be an iunt8array instead an url too...
+	 */
 	preloadSample(sampleName: string, url: string): Promise<boolean>;
 
 }
@@ -15,26 +35,27 @@ export interface PlaybackSettings {
 
 	/**
 	 * Options:
-	 * - 0 no loop
-	 * - -1 continious loop
+	 * - 0 no loop (play once)
+	 * - -1 continuous loop
 	 * - n number of loops
 	 * TODO split this up?
 	 */
 	loopCount?: number;
 
 	/**
-	 * pan audio left/right, 0 means even balanced, -1 is full left, 1 to full right
+	 * pan audio left/right
+	 * ranges from  -1.0 (left), 0.0 (both) to 1.0 (right)
 	 */
 	pan?: number;
 
 	/**
-	 * pitch audio TODO
+	 * pitch audio
+	 * can be positive or negative and directly adds onto the standard sample frequency
 	 */
 	pitch?: number;
 
 	/**
 	 * define audio volume between 0.0 and 1.0
-	 * TODO: does the vpinmame use setting between 0.0 .. 3.0 ?
 	 */
 	volume?: number;
 
@@ -44,16 +65,18 @@ export interface PlaybackSettings {
 	randomPitch?: number;
 
 	/**
+	 * sample would reuse the same channel if it is already playing
 	 * TODO: i don't have a clue yet
+	 */
+	useExisting?: boolean;
+
+	/**
+	 * useful if useExisting is set to true - restart the sample from beginning
 	 */
 	restart?: boolean;
 
 	/**
-	 * TODO: i don't have a clue yet
-	 */
-	useSame?: boolean;
-
-	/**
+	 * similar to pan but fades between the front and rear speakers
 	 * TODO: i don't have a clue yet
 	 */
 	frontRearFade?: number;
