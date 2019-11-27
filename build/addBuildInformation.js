761a291135a50066d9188f3f919258ef50c92312
michuNEEO <michael@neeo.com>
2019-11-27 23:56:22 +0100

    add git information

--- build/addBuildInformation.js
+++ build/addBuildInformation.js
@@ -4,13 +4,18 @@
 
 const path = require('path');
 const fs = require('fs');
+const childProcess = require('child_process');
 
 const timestamp = Date.now();
 const version = require('../package.json').version;
+const gitHash = childProcess.execSync('git rev-parse --short HEAD').toString().trim();
+const gitBranch = childProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 
 const targetFile = path.join(__dirname, '..', 'dist', 'build.json');
 const buildInformation = {
 	version,
 	timestamp,
+	gitHash,
+	gitBranch,
 };
 fs.writeFileSync(targetFile, JSON.stringify(buildInformation));
